ce510cb7b6531b03ea8edb968e632b6736cfc60b
Yogendra Chauhan <yogendra.chauhan@bcone.com>
2018-04-09 11:42:08 +0530

    folder structure changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './modules/dashboard/dashboard.component';
-import { APPROUTES } from './app.route.module';
-import { LoginComponent } from './modules/login/login.component';
-import { LoginModule } from './modules/login/login.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    APPROUTES,
-    LoginModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [ LoginComponent ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
